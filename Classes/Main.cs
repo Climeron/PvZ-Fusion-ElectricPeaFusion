74bae2044c9185882500dbd30bbcd799337928e0
Climeron <climeron@ya.ru>
2024-12-03 00:13:23 +0300

    New repo

---
+++ Classes/Main.cs
@@ -0,0 +1,30 @@
+﻿using MelonLoader;
+using ClimeronToolsForPvZ.Extensions;
+using Il2Cpp;
+using UnityEngine;
+
+namespace ElectricPeaFusion
+{
+    public class Main : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            DefineElectricPeaPrefabs();
+            RepairMixData();
+            EnableElectricPeaHybrid();
+        }
+        private void DefineElectricPeaPrefabs()
+        {
+            GameAPP.plantPrefab[257] = Resources.Load<GameObject>("Plants/Peashooter/Electricpea/Electricpeaprefab");
+            GameAPP.prePlantPrefab[257] = Resources.Load<GameObject>("Plants/Peashooter/Electricpea/Electricpeapreview");
+        }
+        private void RepairMixData() =>
+            MixData.data = Il2CppSystem.Array.CreateInstance(typeof(int).ToIl2CppType(), 2048, 2048);
+        private void EnableElectricPeaHybrid()
+        {
+            Il2CppSystem.Array array = MixData.data.Cast<Il2CppSystem.Array>();
+            array.SetValue(257, 0, 1100);
+            array.SetValue(257, 1100, 0);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(ElectricPeaFusion.Main), ElectricPeaFusion.AssemblyInfo.MODE_NAME, ElectricPeaFusion.AssemblyInfo.VERSION, ElectricPeaFusion.AssemblyInfo.AUTHOR)]
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ElectricPeaFusion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ElectricPeaFusion")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d2b15e2d-a58e-4df4-a1e6-157e61bb1ca3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(ElectricPeaFusion.AssemblyInfo.VERSION)]
+[assembly: AssemblyFileVersion(ElectricPeaFusion.AssemblyInfo.VERSION)]
+
+namespace ElectricPeaFusion
+{
+    public static class AssemblyInfo
+    {
+        public const string MODE_NAME = nameof(ElectricPeaFusion);
+        public const string VERSION = "214.0.0";
+        public const string AUTHOR = "Climeron";
+    }
+}
